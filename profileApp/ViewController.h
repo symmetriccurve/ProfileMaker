fbf176f0f00a509f4821ed36df78265735a57dc0
Belde, Vikram <vikram.belde@one.verizon.com>
2016-08-21 01:55:58 -0400

    Initial Commit

---
+++ profileApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  profileApp
+//
+//  Created by Belde, Vikram on 8/21/16.
+//  Copyright © 2016 com.verizon.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ profileApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  profileApp
+//
+//  Created by Belde, Vikram on 8/21/16.
+//  Copyright © 2016 com.verizon.www. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
